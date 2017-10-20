ba9642786d61923c90c545f03332833122131f6d
George Zhang <george.itg@gmail.com>
2017-10-19 20:21:30 -0600

    more statement

--- sql_join.sql
+++ sql_join.sql
@@ -43,6 +43,9 @@ SELECT Table1.key1, Table1.value1, Table2.key1, Table2.value1
 FROM Table1 inner join Table2 on Table1.key1 >= Table2.key1
 GO
 -- Insert more data and Explore all combinations......
+truncate table table1;
+truncate table table2;
+GO
 
 INSERT INTO [dbo].[Table1] ([key1] ,[value1], [key2] ,[value2]) VALUES(1,'A', 101, 'aa')
 INSERT INTO [dbo].[Table1] ([key1] ,[value1], [key2] ,[value2]) VALUES(2,'B', 102, 'ab')
@@ -56,6 +59,6 @@ INSERT INTO [dbo].[Table2] ([key1] ,[value1], [key2] ,[value2]) VALUES(6,'Z', 10
 GO
 
 -- now use two keys
-SELECT Table1.key1, Table1.value1, Table2.key1, Table2.value1 
+SELECT Table1.*, Table2.* 
 FROM Table1 inner join Table2 on Table1.key1 = Table2.key1 and Table1.key2 = Table2.key2
 GO
